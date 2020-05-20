7c7ba3d72e4240fe83ca75446cf854751c16983e
Pavel Korchagin <mortalis94@gmail.com>
2020-05-20 18:14:37 +0300

    Реализован "посев" данных в БД

--- init_db.py
+++ init_db.py
@@ -1,6 +1,10 @@
+import secrets
+
+from passlib.hash import sha256_crypt
 from sqlalchemy import MetaData, create_engine
 
-from shop.db import order, order_product, product, user, user_order
+from shop.db import (Gender, order_product_table, order_table,
+                     product_table, token_table, user_order_table, user_table)
 from shop.settings import config
 
 DSN = 'postgresql://{user}:{password}@{host}:{port}/{database}'
@@ -8,17 +12,54 @@ DSN = 'postgresql://{user}:{password}@{host}:{port}/{database}'
 
 def create_tables(engine):
     meta = MetaData()
-    tables = [user, product, order, order_product, user_order]
+    tables = [
+        user_table, token_table, product_table,
+        order_table, order_product_table, user_order_table
+    ]
     meta.create_all(bind=engine, tables=tables)
 
 
-def sample_data(engine):
-    pass
+def insert_users(conn):
+    conn.execute(user_table.insert(), [{
+        'login': 'admin',
+        'password': sha256_crypt.hash('admin'),
+        'first_name': 'Ivan',
+        'surname': 'Ivanov',
+        'middle_name': 'Ivanovich',
+        'sex': Gender.male,
+        'age': 25
+    }, {
+        'login': 'user1',
+        'password': sha256_crypt.hash('user1'),
+        'first_name': 'Petr',
+        'surname': 'Petrov',
+        'middle_name': 'Petrovich',
+        'sex': Gender.male,
+        'age': 26
+    }])
+
+
+def insert_tokens(conn):
+    tokens = []
+    users = conn.execute(user_table.select())
+    for user in users:
+        tokens.append({
+            'user_id': user.id,
+            'token': secrets.token_hex(32)
+        })
+
+    conn.execute(token_table.insert(), tokens)
+
+
+def seed_db(engine):
+    with engine.connect() as conn:
+        insert_users(conn)
+        insert_tokens(conn)
 
 
 if __name__ == '__main__':
     db_url = DSN.format(**config['postgres'])
-    engine = create_engine(db_url)
+    db_engine = create_engine(db_url)
 
-    create_tables(engine)
-    sample_data(engine)
+    create_tables(db_engine)
+    seed_db(db_engine)
