64c5e252968e74539c692f8edb2b6c5d3e78f417
Pavel Korchagin <mortalis94@gmail.com>
2020-05-23 19:19:56 +0300

    Оптимизированы роуты

--- shop/routes.py
+++ shop/routes.py
@@ -1,8 +1,10 @@
-from shop.views import index, LoginView, ProductListCreateView
+from shop import views
+from aiohttp import web
 
 
 def setup_routes(app):
-    app.router.add_get('/', index)
-    app.router.add_post('/login', LoginView)
-    app.router.add_get('/products', ProductListCreateView)
-    app.router.add_post('/products', ProductListCreateView)
+    app.router.add_routes([
+        web.get('/', views.index),
+        web.post('/login', views.LoginView),
+        web.view('/products', views.ProductListCreateView)
+    ])
